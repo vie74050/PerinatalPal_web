2c0f9587b871e15b2ee9ed7ab4f0ca9127b648dd
vie_bcit <vienna_ly@bcit.ca>
2024-07-04 17:52:49 -0700

    + ability to specify alternative title for each button label

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,18 @@
 
 // receives message from Unity on app start for UI setup
 function FromUnity_ApplicationStarted(s) {
+  // Use these titles for the buttons. The key is the string sent from Unity for button label
+  let titles = {
+    "OP": "Occipito-posterior",
+    "OA": "Occipito-anterior",
+    "ROT": "Right occipito-transverse",
+    "LOT": "Left occipito-transverse",
+    "ROA": "Right occipito-anterior",
+    "LOP": "Left occipito-posterior",
+    "LOT": "Left occipito-transverse",
+    "ROP": "Right occipito-posterior",
+  }
+
   console.log("FromUnity_ApplicationStarted", s);
   // expects a comma separated string for the button labels
   var strArray = s.split(",");
@@ -8,8 +20,10 @@ function FromUnity_ApplicationStarted(s) {
   // create buttons
   var nav = document.querySelector(".nav");
   if (nav) {
-    for (var i = 0; i < strArray.length; i++) {
-      var button = CreateButton(strArray[i], i);
+    for (let i = 0; i < strArray.length; i++) {
+      let label = strArray[i];
+      let title = titles[label]? titles[label] : ""; console.log("title", titles[label], title, label);
+      let button = CreateButton(label, i, title);
       nav.appendChild(button);
 
       if (i==0){
